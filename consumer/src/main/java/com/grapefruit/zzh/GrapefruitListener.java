69ce57d6af77f701988d5da7acab019f4e8f57cb
zzh-1992 <ezzh1992@icloud.com>
2021-04-27 22:26:31 +0800

    add kafka provider and comsumer

---
+++ consumer/src/main/java/com/grapefruit/zzh/GrapefruitListener.java
@@ -0,0 +1,30 @@
+package com.grapefruit.zzh;
+
+import com.grapefruit.zzh.log.LogTools;
+import com.grapefruit.zzh.receiver.KafkaListener;
+import com.grapefruit.zzh.receiver.KafkaMapping;
+import org.slf4j.Logger;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * kafka消费者(普通接口调用及MQ消息框架调用)
+ *
+ * @author Grapefruit
+ * @version 1.0
+ * @date 2021/1/31
+ */
+@KafkaListener
+@RequestMapping("/")
+@RestController
+public class GrapefruitListener {
+    private final static Logger log = LogTools.getReceiveLogger();
+
+    @KafkaMapping(topic = "grapefruit")
+    @GetMapping("/onMessage")
+    public void onMessage(String key, String msg) {
+        log.debug("receive msg:{}", msg);
+    }
+}
---
+++ provider/src/main/java/com/grapefruit/zzh/MsgProvider.java
@@ -0,0 +1,28 @@
+package com.grapefruit.zzh;
+
+import com.grapefruit.zzh.sender.KafkaSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * kafka消息提供者
+ *
+ * @author Grapefruit
+ * @version 1.0
+ * @date 2021/4/22
+ */
+@RequestMapping("/")
+@RestController
+public class MsgProvider {
+    /**
+     * kafka发送工具类
+     */
+    @Autowired
+    KafkaSender sender;
+
+    @RequestMapping("{send}")
+    public String index(String msg) {
+        return sender.send(msg);
+    }
+}
